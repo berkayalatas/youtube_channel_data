ea1ae44ab9e706b50c7ded324a91de1e79848f00
Berkay Alatas <berkay.3304@gmail.com>
2020-10-31 15:46:21 +0300

    channel update

--- main.js
+++ main.js
@@ -67,15 +67,44 @@ function handleSignoutClick() {
     gapi.auth2.getAuthInstance().signOut();
 }
 
-// get channel from api
+//displa channel data
+function showChannelData(data) {
+    const channelData = document.getElementById('channel-data');
+    channelData.innerHTML = data;
+}
 
+
+// get channel from api
 function getChannel(channel) {
     gapi.client.youtube.channels.list({
-        part:'snippet,contentDetails,statistics',
-        forUsername:channel
-    })
-    .then(Response => {
-        console.log(Response);
-    })
-    .catch(err => alert('No channel by that name'))
+            part: 'snippet,contentDetails,statistics',
+            forUsername: channel
+        })
+        .then(response => {
+            console.log(response);
+            const channel = response.results.items[0];
+
+            const output = `
+        <ul class="collection">
+          <li class="collection-item">Title: ${channel.snippet.title}</li>
+          <li class="collection-item">ID: ${channel.id}</li>
+          <li class="collection-item">Subscribers: ${numberWithCommas(
+            channel.statistics.subscriberCount
+          )}</li>
+          <li class="collection-item">Views: ${numberWithCommas(
+            channel.statistics.viewCount
+          )}</li>
+          <li class="collection-item">Videos: ${numberWithCommas(
+            channel.statistics.videoCount
+          )}</li>
+        </ul>
+        <p>${channel.snippet.description}</p>
+        <hr>
+        <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${
+          channel.snippet.customUrl
+        }">Visit Channel</a>
+      `;
+      showChannelData(output);
+        })
+        .catch(err => alert('No channel by that name'))
 }
